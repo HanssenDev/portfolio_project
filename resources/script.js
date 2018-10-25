5055220c60796e2c4dc8970567baf618f63264c8
HanssenDev <41926589+HanssenDev@users.noreply.github.com>
2018-10-25 15:35:23 +0100

    Added jQuery code
    jQuery code with waypoint

--- resources/script.js
+++ resources/script.js
@@ -1,9 +1,40 @@
-//$(document).ready(function() {
-//    
-//   $('h1') .click(function () {
-//       $(this).css('background-color', '#ff0000');
-//   })
-//    
-//    
-//});
-//                  
+$(document).ready(function() {
+    
+    /* For the sticky navigation */
+    $('.js--section-projects').waypoint(function(direction) {
+        if (direction == "down") {
+            $('header nav').addClass('sticky')
+        } else {
+            $('header nav').removeClass('sticky')
+        }
+    }, {
+        offset: '150px;'
+    });
+    
+    
+    
+    
+    /* For the scroll on buttons */
+    
+    
+    
+    
+    
+    
+    /* For the scroll on links */
+    
+    
+    
+    
+    
+    /* For the animations on scroll */
+    
+    
+    
+    
+    
+    /* For the responsive navigation */
+    
+    
+});
+                  
